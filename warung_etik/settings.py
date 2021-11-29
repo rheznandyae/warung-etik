ef4dce1319da2baf324c95d507fc1a72f3a34951
rheznandyae <rheznandya.erwanto@gmail.com>
2021-11-29 21:09:41 +0800

    Init app

--- warung_etik/settings.py
+++ warung_etik/settings.py
@@ -56,6 +56,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'barang',
+    'chat',
+    'keranjang',
+    'admin',
+    'transaksi'
 ]
 
 MIDDLEWARE = [
