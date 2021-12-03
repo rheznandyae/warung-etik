fd555df9c66cb4625b2eb7421a616442d7b3cc82
Farel Muhammad Daffa <farelmuhdf@gmail.com>
2021-12-03 16:32:12 +0700

    Add Barang model

--- admin_warung/models.py
+++ admin_warung/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Barang(models.Model):
+    nama = models.CharField(max_length=100)
+    id_barang = models.IntegerField()
+    harga = models.PositiveIntegerField()
+    stok = models.PositiveIntegerField()
+    terjual = models.PositiveIntegerField()
+    deskripsi = models.TextField()
