9956d85cd7ca45dd8d7f3d2e6d5d0aa439c81132
Ikhsan Asa P <ikhsan.pambayun@gmail.com>
2021-12-04 15:20:53 +0700

    add model transaksi and finish method choice

--- transaksi/views.py
+++ transaksi/views.py
@@ -1,7 +1,54 @@
 from django.http import response
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.utils import timezone
+
+from .models import Transaksi
+
+# Create your views here.
+
 
 def pembayaran(request):
+
+    if request.method == 'POST':
+
+        metode = request.POST.get('metode')
+
+        lastTR = Transaksi.objects.last()
+
+        if lastTR == None:
+            newID = 'TR00001'
+
+        else:
+            lastID = int(lastTR.idTransaksi[2:])
+            # print(lastID)
+            newID = lastID + 1
+            # print(newID)
+
+            if newID < 10:
+                newID = 'TR0000' + str(newID)
+            elif newID < 100:
+                newID = 'TR000' + str(newID)
+            elif newID < 1000:
+                newID = 'TR00' + str(newID)
+            elif newID < 10000:
+                newID = 'TR0' + str(newID)
+            else:
+                newID = 'TR' + str(newID)
+
+        newTR = Transaksi(
+            idTransaksi = newID,
+            statusTransaksi = 'menunggu konfirmasi pembayaran',
+            usernamePembeli = 'tes',
+            caraPembayaran = metode,
+            timeStamp = timezone.now()
+        )
+
+        newTR.save()
+
+        # print(newID)
+    
+        return redirect('transaksi:transaksiBayar')
+    
     context = {}
     return render(request, 'pembayaran.html', context=context)
 
@@ -21,4 +68,3 @@ def transaksiFailTemp(request):
     context = {}
     return render(request, 'transaksi-failtemp.html', context=context)
 
-# Create your views here.
