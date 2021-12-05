78254f4ab5bfc9e176de47b78d7cfcba21f1005f
Arllivandhya Dani <arllivandhya.dani@ui.ac.id>
2021-12-05 22:56:59 +0700

    fix: detail barang

--- visualisasi_data/views.py
+++ visualisasi_data/views.py
@@ -1,7 +1,7 @@
 from django.http import response
 from django.shortcuts import render
 from django.views.generic import TemplateView
-#from .models import Transaksi
+from django.db.models import Count
 from transaksi.models import Transaksi
 from keranjang.models import ItemKeranjang
 from admin_warung.models import Barang
@@ -11,7 +11,17 @@ class TransaksiChartView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["transaksi"] = Transaksi.objects.all()
-        context["barang"] = Barang.objects.all()
-        context["item_keranjang"] = ItemKeranjang.objects.all()
+        list_transaksi = Transaksi.objects.all()
+        list_barang = Barang.objects.all()
+        list_item_keranjang = ItemKeranjang.objects.all()
+
+        
+        
+        transaksi_group_by_cara_pembayaran = Transaksi.objects.values('caraPembayaran').order_by('caraPembayaran').annotate(count=Count('caraPembayaran'))
+        transaksi_group_by_status = Transaksi.objects.values('statusTransaksi').order_by('statusTransaksi').annotate(count_status=Count('statusTransaksi'))
+        context["status_transaksi"] = transaksi_group_by_status
+        context["cara_pembayaran"] = transaksi_group_by_cara_pembayaran
+        context["transaksi"] = list_transaksi
+        context["barang"] = list_barang
+        context["item_keranjang"] = list_item_keranjang
         return context
