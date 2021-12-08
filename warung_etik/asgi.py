599c7eb8e40afe172875115455d8472e9ad2d6fe
YasyfaWiwaha <79443651+YasyfaWiwaha@users.noreply.github.com>
2021-12-08 20:42:07 +0700

    edit asgi.py

--- warung_etik/asgi.py
+++ warung_etik/asgi.py
@@ -4,10 +4,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import django
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "warung_etik.settings")
 django.setup()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
