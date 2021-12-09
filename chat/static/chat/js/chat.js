87e703435fa3b16bea01928598fbf5251acbe2a0
YasyfaWiwaha <79443651+YasyfaWiwaha@users.noreply.github.com>
2021-12-09 12:20:28 +0700

    Improve chat timestamp look at frontend

--- chat/static/chat/js/chat.js
+++ chat/static/chat/js/chat.js
@@ -71,7 +71,7 @@ function createMessage(data) {
     div2Msg.textContent = message;
 
     liMsg.className = 'clearfix';
-    spanMsg.className = "message-data-time";
+    spanMsg.className = getDateTime(new Date());
 
     if (author === username) {
         div1Msg.className = 'message-data';
@@ -86,4 +86,30 @@ function createMessage(data) {
     div1Msg.appendChild(spanMsg)
 
     document.querySelector('#chat-log').appendChild(liMsg);
+}
+
+function getDateTime(date) {
+    // gets the hours
+    var hours = date.getHours();
+    // gets the day
+    var days = date.getDay();
+    // gets the month
+    var minutes = date.getMinutes();
+    // gets AM/PM
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    // converts hours to 12 hour instead of 24 hour
+    hours = hours % 12;
+    // converts 0 (midnight) to 12
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    // converts minutes to have leading 0
+    minutes = minutes < 10 ? '0'+ minutes : minutes;
+  
+    // the time string
+    var time = hours + ':' + minutes + ' ' + ampm;
+  
+    // gets the match for the date string we want
+    var match = date.toString().match(/\w{3} \w{3} \d{1,2} \d{4}/);
+  
+    //the result
+    return match[0] + ' ' + time;
 }
